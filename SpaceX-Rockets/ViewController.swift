e9afdffe586b904643afdc107a939e9a71de6559
Pavel Valadzko <pavel.valadzko@gmail.com>
2022-08-23 15:15:18 +0300

    Hardcoded UI template for rocket info

--- SpaceX-Rockets/ViewController.swift
+++ SpaceX-Rockets/ViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var movies: [String] = ["bad-boys.jpeg","joker.jpeg","hollywood.jpeg"]
-    var frame = CGRect.zero
        
     override func viewDidLoad() {
         super.viewDidLoad()
