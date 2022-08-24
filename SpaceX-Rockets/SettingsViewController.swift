4249080751b1c623c5796000731772e166880b18
Pavel Valadzko <pavel.valadzko@gmail.com>
2022-08-24 15:57:52 +0300

    Complete Setting view, initial view for rocket launch history

---
+++ SpaceX-Rockets/SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  SpaceX-Rockets
+//
+//  Created by Pavel on 24.08.22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButtonPressed(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SpaceX-Rockets/ViewController.swift
+++ SpaceX-Rockets/ViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-       
+    @IBOutlet weak var rockerInfoView: UIView!
+    
+    @IBAction func settingButtonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        rockerInfoView.layer.cornerRadius = 40
+        rockerInfoView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
     }
- 
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 }
